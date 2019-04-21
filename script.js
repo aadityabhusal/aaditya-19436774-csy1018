f012a05d284b70370c1ed7ee42029e0d5c4c6ce1
aadityabhusal <bhusal.001aditya@gmail.com>
2019-04-21 17:27:54 +0545

    All Pages Made Responsive

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
-function contactValidation(){
-
-	var inputElement = document.getElementsByClassName('material-input');
-
-	for (var i = 0; i < inputElement.length; ++i) {
-		inputElement[i].onblur = function(){
-			if(this.value == ''){
-				this.style.borderBottom = "3px solid #e74c3c";
-			}else{
-				this.style.borderBottom = "1px solid #bdc3c7";
-			}
-		}
-	    
+function validate(field){
+	if(field.value == ''){
+		field.style.borderBottom = "3px solid #e74c3c";
+	}else{
+		field.style.borderBottom = "1px solid #bdc3c7";
 	}
 }
-contactValidation();
+
+var menu = document.getElementById('menu');
+function menuOpen(){
+	menu.style.transform = "translate(0,0)";
+}
+function menuClose(){
+	menu.style.transform = "translate(-120vw,0)";
+}
