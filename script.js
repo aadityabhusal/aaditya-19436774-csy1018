0f637bc6cec1b495e3326632d9f36596a5d51e13
aadityabhusal <bhusal.001aditya@gmail.com>
2019-05-12 17:19:09 +0545

    Final Commit

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
-function validate(field){
-	if(field.value == ''){
-		field.style.borderBottom = "3px solid #e74c3c";
+
+function validate(field){// Validate function Starts
+	if(field.value == ''){ //Checking if the field is empty
+		field.style.borderBottom = "3px solid #e74c3c"; //Change the border color to red
 	}else{
-		field.style.borderBottom = "1px solid #bdc3c7";
+		field.style.borderBottom = "1px solid #bdc3c7"; //Change the border color to grey
 	}
 }
+// Validate function Ends
 
-var menu = document.getElementById('menu');
-function menuOpen(){
-	menu.style.transform = "translate(0,0)";
+
+var menu = document.getElementById('menu');//Menu Section translation starts
+function menuOpen(){ //Menu open function
+	menu.style.transform = "translate(0,0)"; //Display the menu
 }
-function menuClose(){
-	menu.style.transform = "translate(-120vw,0)";
+function menuClose(){//Menu close function
+	menu.style.transform = "translate(-120vw,0)";//hide the menu
 }
+//Menu Section translation starts
 
-var fullDate = new Date();
-var date = (fullDate+"").slice(4,15);
-document.getElementById('date').innerHTML = date;
+//Date display code starts
+var fullDate = new Date(); //Using the javascript date function
+var date = (fullDate+"").slice(4,15); //Slicing the the whole date to show date month and year only
+document.getElementById('date').innerHTML = date;//displaying the date
+//Date display code ends
 
-var copyrightDate = new Date().getFullYear();
-document.getElementById('copyright-year').innerHTML = copyrightDate;
+//copyright display starts
+var copyrightDate = new Date().getFullYear();//Using the javascript date function and full year function 
+document.getElementById('copyright-year').innerHTML = copyrightDate;//displaying the year
+//copyright display ends
